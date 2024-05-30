389ccee454eb2b7681f77cbe4bd43600df0eabdb
forest-lynx <trot505@yandex.ru>
2024-05-30 06:42:31 +0400

    feat: :building_construction: Структура пакета

---
+++ src/InputExtensions/Calculator.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ForestLynx\MoonShine\InputExtensions;
+
+use MoonShine\InputExtensions\InputExtension;
+
+class Calculator extends InputExtension
+{
+    protected string $view = 'moonshine-fl::input-extensions.calculator';
+}
---
+++ src/Providers/CalculatorServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ForestLynx\MoonShine\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+final class CalculatorServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'moonshine-fl');
+        $this->publishes([
+            __DIR__ . '/../../public' => public_path('vendor/moonshine-input-extension-calculator'),
+        ], ['moonshine-input-extension-calculator', 'laravel-assets']);
+
+        moonshineAssets()->add([
+            '/vendor/moonshine-input-extension-calculator/css/flCalculator.css',
+            '/vendor/moonshine-input-extension-calculator/js/flCalculator.js',
+         ]);
+    }
+}
